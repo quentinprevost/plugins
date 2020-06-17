027300ccb6eb1a19161e74e56ce3a00f678cd664
quentin <quentinprevost14@gmail.com>
2020-06-17 02:59:24 +0200

    u

--- packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
@@ -476,7 +476,7 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
 - (void)initialize:(FlutterError* __autoreleasing*)error {
   // Allow audio playback when the Ring/Silent switch is set to silent
   [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions: AVAudioSessionCategoryOptionMixWithOthers|AVAudioSessionCategoryOptionDuckOthers error:nil];
-    [[AVAudioSession sharedInstance] setActive:false error:nil];
+    [[AVAudioSession sharedInstance] setActive:true error:nil];
   for (NSNumber* textureId in _players) {
     [_registry unregisterTexture:[textureId unsignedIntegerValue]];
     [_players[textureId] dispose];
